8c37e4f3c5d8f96e937f00234937c3d4c9edc603
Chow Yin Hao <yinhaoc10@gmail.com>
2026-04-14 20:35:10 +0800

    Auth: Implemented login page UI and basic route handling

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+from flask import Flask, render_template, request, redirect, url_for
+# We will add Flask-Login later, for now just render the page
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # This is where your logic will go later to check passwords
+        return redirect(url_for('home'))
+    return render_template('login.html')
